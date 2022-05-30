35e289d1a44a7bf9bba68941a1666d39a01b5c53
Brayan-code1234 <77586737+Brayan-code1234@users.noreply.github.com>
2022-05-29 20:48:23 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -22,9 +22,19 @@ var ball = {
 }
 
 function setup(){
-  var canvas =  createCanvas(700,600);
+  canvas =  createCanvas(700,600);
+  canvas.parent('canvas');
+
+  video = createCapture(VIDEO);
+  video.size(700, 600);
+  video.hide();
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
 }
 
+function modelLoaded(){
+  console.log("modelLoaded");
+}
 
 function draw(){
 
